8cded9cdbf4cda38187dcfd48ddc4976b9605b1e
Hugo Meza <hugo2mz@gmail.com>
2023-02-20 10:58:22 -0500

    First commit

---
+++ src/test/java/dev/hugomeza/airportdemo/AirportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hugomeza.airportdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
